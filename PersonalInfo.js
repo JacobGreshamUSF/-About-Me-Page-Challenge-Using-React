09a9d5a437de67ae25158338dce774a86ac553e1
JacobGreshamUSF <greshamj@usf.edu>
2024-07-01 13:13:32 -0400

    Create PersonalInfo.js

---
+++ PersonalInfo.js
@@ -0,0 +1,16 @@
+//U59555732
+import React from 'react';
+import './PersonalInfo.css';
+
+const PersonalInfo = () => {
+  return (
+    <div className="personal-info">
+      <h1>Jacob Gresham</h1>
+      <p>Software Engineer with a passion for developing innovative programs. Skilled in various programming languages and technologies.</p>
+      <p>Email: greshamj@usf.edu</p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/jacobgresham/">jacob gresham</a></p>
+    </div>
+  );
+};
+
+export default PersonalInfo;
